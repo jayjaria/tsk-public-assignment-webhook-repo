15594ac87fc58816220b083d0bc84dc3c18c5ba4
jayjaria <jaijaria032001@gmail.com>
2024-04-11 23:23:40 +0530

    Webhook repo Integration with Action repo and MongoDb

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,16 @@
 from flask import Flask
-
+from app.extensions import mongo
 from app.webhook.routes import webhook
+from flask_cors import CORS
 
 
 # Creating our flask app
 def create_app():
-
     app = Flask(__name__)
-    
+
     # registering all the blueprints
     app.register_blueprint(webhook)
-    
+    app.config["MONGO_URI"] = "mongodb://localhost:27017/database"
+    mongo.init_app(app)
+    CORS(app)
     return app
--- app/webhook/routes.py
+++ app/webhook/routes.py
@@ -1,7 +1,17 @@
-from flask import Blueprint, json, request
+from flask import Blueprint, request, json
+from app.extensions import mongo
+from bson import json_util
 
-webhook = Blueprint('Webhook', __name__, url_prefix='/webhook')
+webhook = Blueprint("Webhook", __name__, url_prefix="/webhook")
 
-@webhook.route('/receiver', methods=["POST"])
+
+@webhook.route("/receiver", methods=["POST"])
 def receiver():
+    mongo.db.webhook.insert_one(request.json)
     return {}, 200
+
+
+@webhook.route("/get_all_messages", methods=["GET"])
+def get_msgs():
+    msgs = mongo.db.webhook.find()
+    return json.loads(json_util.dumps(msgs))
